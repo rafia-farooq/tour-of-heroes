10d2dcef7968c7f4eec31d04187ba3cb9e58fee1
r-farooq <rafiafarooq@hotmail.com>
2025-05-09 23:52:07 +0300

    clicking on a hero shows details - event and class binding

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -11,9 +11,17 @@ import { HEROES} from "../hero-list"
   styleUrl: './heroes.component.css'
 })
 export class HeroesComponent {
-  hero : Hero = {
-    id : 1,
-    name : "Windstorm"
-  };
+  // static test hero
+  // hero : Hero = {
+  //   id : 1,
+  //   name : "Windstorm"
+  // };
+
   heroes = HEROES;
+  selectedHero? : Hero;
+
+  // method to show hero details on click
+  onSelect(hero: Hero) : void{
+    this.selectedHero = hero
+  }
 }
