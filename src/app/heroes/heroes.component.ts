f0698c82438249b9c3a60086e45aed23f3cbfc3e
r-farooq <rafiafarooq@hotmail.com>
2025-05-09 17:45:40 +0300

    two-way binding to update hero name

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,10 +1,11 @@
 import {CommonModule} from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; // Import FormsModule for NgModel
 import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
-  imports: [ CommonModule ],
+  imports: [ CommonModule, FormsModule ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css'
 })
