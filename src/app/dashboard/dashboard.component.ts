d8bd1cb92166617fa4f048e57916b16828d8dc0e
r-farooq <rafiafarooq@hotmail.com>
2025-05-12 19:43:09 +0300

    add dashboard view and a few heroes there

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [ CommonModule ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
+  heroes: Hero[] = [];
 
+  constructor(private heroService : HeroService) { }
+ 
+  getHeroes() : void {
+    this.heroService.getHeroes()
+    .subscribe(list => this.heroes = list.slice(1, 5));
+  }
+
+  ngOnInit() : void {
+    this.getHeroes()
+  }
 }
