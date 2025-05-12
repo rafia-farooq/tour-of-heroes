1f0d58b5ff8957f7c161e37d473ad0cb250226b8
r-farooq <rafiafarooq@hotmail.com>
2025-05-12 19:20:57 +0300

    added router and navigation for heroes page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavComponent } from './nav/nav.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { MessagesComponent } from './messages/messages.component';
-
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, NavComponent, HeroesComponent, MessagesComponent],
+  imports: [CommonModule, NavComponent, MessagesComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
